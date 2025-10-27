ce0166d6aacaea302c305ea0d039450cb28b3d2b
mohamed-passau-deggendorf <rahmouni_mohamed.1@yahoo.com>
2025-10-27 22:07:13 +0100

    "iflist.c" adding network interface type with threading

--- iflist.c
+++ iflist.c
@@ -18,19 +18,32 @@ for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)   if (ifa->ifa_addr->sa_fam
 	 char ip[INET_ADDRSTRLEN];
 	char mask[INET_ADDRSTRLEN];
 	char brks[INET_ADDRSTRLEN];
-	int mac_fd = socket(AF_INET, SOCK_DGRAM, 0);
+	int mac_fd = socket(AF_INET, SOCK_DGRAM, 0); 
 	struct ifreq ifr;
         strncpy(ifr.ifr_name, ifa->ifa_name, IFNAMSIZ-1);
 	ioctl(mac_fd, SIOCGIFHWADDR, &ifr);
 	unsigned char *mac = (unsigned char *)ifr.ifr_hwaddr.sa_data;
+	ioctl(mac_fd, SIOCGIFHWADDR, &ifr);
+	char arp_hw[256];
+	void* threading_hw[1004];
+
+	threading_hw[ARPHRD_ETHER] = &&ether_threading;
+	threading_hw[ARPHRD_IEEE80211] = &&iee81211_threading;
+	threading_hw[ARPHRD_LOOPBACK] = &&loopback_threading;
+
+	goto *(threading_hw[ifr.ifr_hwaddr.sa_family]);
+	ether_threading : strcpy(arp_hw,"Ethernet"); goto end_threading;
+	iee81211_threading :  strcpy(arp_hw,"Wireless"); goto end_threading;
+	loopback_threading :   strcpy(arp_hw,"Loopback"); end_threading: ;
+
 	inet_ntop(AF_INET, &( (struct sockaddr_in *)ifa->ifa_ifu.ifu_broadaddr)->sin_addr, brks, sizeof(brks));
 
 
-	printf("%s %x %s %s  %02x:%02x:%02x:%02x:%02x:%02x (%d) \n",
+	printf("%s %x %s %s  %02x:%02x:%02x:%02x:%02x:%02x %s  \n",
 		ifa->ifa_name, ifa->ifa_flags,
 		inet_ntop(AF_INET, &( (struct sockaddr_in *)ifa->ifa_addr)->sin_addr, ip, sizeof(ip)),
 		inet_ntop(AF_INET, &( (struct sockaddr_in *)ifa->ifa_netmask)->sin_addr, mask, sizeof(mask)),
-		mac[0], mac[1], mac[2], mac[3], mac[4], mac[5] );	
+		mac[0], mac[1], mac[2], mac[3], mac[4], mac[5] , arp_hw );	
 
 		close(mac_fd);
 
